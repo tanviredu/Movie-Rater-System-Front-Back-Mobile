53a8fd082d9945bd1f45674e99aaa97b4178dc48
tanviredu <tanviredu2018@gmail.com>
2023-11-11 22:19:42 +0600

    added

--- backend/BookProject/bookapp/urls.py
+++ backend/BookProject/bookapp/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
 from rest_framework import routers
 from bookapp.views import BookViewSet
+
+
 router = routers.DefaultRouter()
 router.register("books",BookViewSet)
+
+
 urlpatterns = [
     
 ]
